db70a5a5517d90ee636730a71c5b20a7fb7751f2
afifasoft <abdul.pesit@gmail.com>
2021-02-10 08:47:26 +0530

    Express.js server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
