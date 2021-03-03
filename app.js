a458cd4646e801a37908c895f411dce7a007bb61
afifasoft <abdul.pesit@gmail.com>
2021-03-03 08:43:26 +0530

    Using Express Router

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/', (req, res, next) => {
-    console.log('This always runs!');
+   // console.log('This always runs!');
     next();
 })
 
 app.use('/add-product',(req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>The "Add Product" Page</h1>');
+    //console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
 });
 
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.use('/',(req, res, next) => {
-    console.log('In another middleware');
+   //c console.log('In another middleware');
     res.send('<h1>Hello from Express!</h1>');
 });
 
