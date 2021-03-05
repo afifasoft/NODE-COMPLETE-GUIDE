8e1763d272562e79609c0e2eb87a139560e8f870
afifasoft <abdul.pesit@gmail.com>
2021-03-05 08:21:04 +0530

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -3,30 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-   // console.log('This always runs!');
-    next();
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 })
-
-app.use('/add-product',(req, res, next) => {
-    //console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next) => {
-   //c console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>');
-});
-
 //const server = http.createServer(app);
 
 //server.listen(3000);
