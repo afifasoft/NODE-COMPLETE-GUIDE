0d9be1cacb44d30ab07bab8cfb9ab7a1842f035e
afifasoft <abdul.pesit@gmail.com>
2021-03-05 08:28:59 +0530

    Filtering Paths

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 
