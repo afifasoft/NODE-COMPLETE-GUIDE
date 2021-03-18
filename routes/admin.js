afa50f0a169426968640e2e3945d2f3706f69bdd
afifasoft <abdul.pesit@gmail.com>
2021-03-18 08:15:14 +0530

    Implementing Pug

--- routes/admin.js
+++ routes/admin.js
@@ -6,16 +6,22 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/add-product',(req, res, next) => {
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
     //console.log('In another middleware');
-   // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
+    // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
 
-module.exports = router;
+//module.exports = router;
+
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,15 +3,18 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
+router.get('/', (req, res, next) => {
     //c console.log('In another middleware');
     // res.send('<h1>Hello from Express!</h1>');
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log('shop.js',adminData.products);
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop');
 
- });
+});
 
- module.exports = router;
+module.exports = router;
