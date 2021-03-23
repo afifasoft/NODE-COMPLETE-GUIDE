87c105a3e82e49d6d6be2c70ab89420e5c4a351a
afifasoft <abdul.pesit@gmail.com>
2021-03-23 08:47:28 +0530

    Finishing the Pug Template

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,8 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
     //console.log('In another middleware');
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,8 @@ router.get('/', (req, res, next) => {
     // res.send('<h1>Hello from Express!</h1>');
     console.log('shop.js',adminData.products);
     //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop', path='/'});
 
 });
 
