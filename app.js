a3d690de6bbb5af3b3fa3d0f93f637adb81d520c
afifasoft <abdul.pesit@gmail.com>
2021-04-03 08:50:22 +0530

    Handlebars

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -19,7 +21,7 @@ app.use(shopRoutes);
 
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 //const server = http.createServer(app);
 
--- routes/shop.js
+++ routes/shop.js
@@ -9,12 +9,9 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    //c console.log('In another middleware');
-    // res.send('<h1>Hello from Express!</h1>');
-    console.log('shop.js',adminData.products);
-    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+   
     const products = adminData.products;
-    res.render('shop', {prods: products, docTitle: 'Shop', path='/'});
+    res.render('shop', {prods: products, pageTitle: 'Shop', path:'/'});
 
 });
 
